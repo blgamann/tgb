a0108ff1d8af2fdb02a8891e4c9fffe0ae05c346
0xdagarn <sgnoo@naver.com>
2024-10-02 14:01:35 +0900

    Add service layout

--- app/sections/about.tsx
+++ app/sections/about.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 
 const About: React.FC<{ language: "KO" | "EN" }> = ({ language }) => {
-  const getTitle = () => {
-    return language === "KO" ? "소개" : "About";
-  };
-
   return (
     <section
       id="about"
-      className="h-screen flex flex-col justify-center items-center text-4xl bg-red-500"
+      className="min-h-screen flex flex-col justify-center text-4xl bg-red-500 px-4 sm:px-8 md:px-20"
     >
-      <h2>{getTitle()}</h2>
-      <p className="mt-4 text-2xl">
-        {language === "KO"
-          ? "여기에 소개 내용이 들어갑니다."
-          : "Content about the section goes here."}
-      </p>
+      <div className="flex flex-col justify-center h-full">
+        <div className="mx-auto sm:mx-0">
+          <p className="font-normal text-white text-4xl sm:text-5xl md:text-6xl mb-6">
+            <span className="block mb-3 sm:mb-5">Empowering Innovation</span>
+            <span className="font-medium text-2xl sm:text-3xl md:text-4xl">
+              Leading Platform Service Experts
+            </span>
+          </p>
+          <p className="font-light text-white text-lg sm:text-xl md:text-2xl leading-relaxed sm:text-left">
+            At TXGroup, our ethos is rooted in User-Centered Experience Design,
+            <br />
+            focusing on creating products that resonate meaningfully with users.
+          </p>
+          <button className="mt-12 sm:mt-16 flex items-center justify-center text-lg sm:text-xl px-6 sm:px-8 py-2.5 font-semibold rounded-full bg-[#ffffff33]">
+            Download TXG Profile
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
--- app/sections/service.tsx
+++ app/sections/service.tsx
@@ -5,12 +5,85 @@ const Service: React.FC<{ language: "KO" | "EN" }> = ({ language }) => {
     return language === "KO" ? "서비스" : "Service";
   };
 
+  const Item = ({
+    service,
+    serviceDescription,
+    buttonText,
+    items,
+  }: {
+    service: string;
+    serviceDescription: string;
+    buttonText: string;
+    items: string[];
+  }) => {
+    return (
+      <div>
+        <div className="border-l-2 border-[#00ff02] pl-4 pb-7">
+          <h2 className="text-[#00ff02] font-bold text-[44.4px] mb-4">
+            {service}
+          </h2>
+          <p className="font-normal text-white text-lg leading-[27px]">
+            {serviceDescription}
+          </p>
+        </div>
+
+        <button className="my-10 rounded-[50px] border border-[#0F0] bg-black inline-flex p-[8px_30px] justify-center items-center gap-[10px] text-[20px]">
+          <span>{buttonText}</span>
+        </button>
+
+        <div className="border-l-2 border-[#00ff02] pl-4 pb-7">
+          {items?.map((item) => (
+            <p
+              key={item}
+              className="font-normal text-white text-lg leading-[27px] mb-4"
+            >
+              {item}
+            </p>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <section
       id="service"
-      className="h-screen flex justify-center items-center text-4xl bg-yellow-500"
+      className="h-screen flex justify-center items-center text-4xl bg-black"
     >
-      <h2>{getTitle()}</h2>
+      <Item
+        service={"Consulting"}
+        serviceDescription={
+          "Each project has its own tasks. We analyze your business model through market and industry trend research and provide the foundation for new business through user research and validated technology-based consulting."
+        }
+        buttonText={"Contact Us"}
+        items={[
+          "PMO Consulting",
+          "Market Research",
+          "Stakeholder Interview Analysis",
+          "Key feature & IA for dev",
+          "Fast Prototyping",
+        ]}
+      />
+      <Item
+        service={"Design"}
+        serviceDescription={
+          "We provide user-centered design-driven innovation where top-level planning and strategy are integrated into each layer, allowing functionality and aesthetics to work together harmoniously, rather than merely pursuing aesthetically pleasing designs."
+        }
+        buttonText={"Portfolio"}
+        items={["CX Design", "BX Design", "UI/UX Design", "Design Systems"]}
+      />
+      <Item
+        service={"Education"}
+        serviceDescription={
+          "We help develop insights that can be applied to real-world problems, moving away from traditional education focused solely on conventional theories and methodologies. We provide guidelines that can be directly applied in enabling individuals to cultivate practical insights."
+        }
+        buttonText={"Curriculum Download"}
+        items={[
+          "Regular Course: Service Planning Job Training",
+          "Seminar: Understanding the Product Owner",
+          "1:1 Coaching: For Service Planning Team Leaders",
+        ]}
+      />
     </section>
   );
 };
