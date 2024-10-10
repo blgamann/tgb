3f68751c2e5d33221df75c273c9ec1fa458602e3
tishoo-geonwoo <geonwoo@tishoo.com>
2024-10-10 11:07:39 +0900

    Fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Script from "next/script";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://www.txgroup.biz";
+
 export const metadata: Metadata = {
   title: "TXGroup | IT 프로젝트컨설팅",
   description:
@@ -10,12 +12,12 @@ export const metadata: Metadata = {
     title: "TXGroup | IT 프로젝트컨설팅",
     description:
       "TXGroup은 진정한 온라인 플랫폼 전문가 집단으로 구성되어 있습니다. 어떤 프로젝트든 저마다 과제를 지니고 있습니다. TXGroup은 티슈다운 접근으로, 사용자 중심의 재해석을 통해 사용자의 특별한 의미를 찾고 기획기반 디자인으로 혁신을 이뤄내도록 돕습니다.",
-    url: process.env.NEXT_PUBLIC_BASE_URL,
+    url: baseUrl,
     siteName: "TXGroup",
     type: "website",
     images: [
       {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}/og_image.png`,
+        url: `${baseUrl}/og_image.png`,
         width: 564,
         height: 564,
         alt: "TXGroup Open Graph Image",
